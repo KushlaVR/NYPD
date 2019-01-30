adb3bf7df5a4f1472467c76338f6ca33def5627c
Виталий Кушла <KushlaVR@gmail.com>
2019-01-30 20:20:00 +0200

    Create NYPD

---
+++ NYPD/NYPD.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		NYPD.ino
+ Created:	1/30/2019 8:18:31 PM
+ Author:	Віталік
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
